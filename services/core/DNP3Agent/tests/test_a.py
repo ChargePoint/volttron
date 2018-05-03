1d9a7b6bf29dcac63d9250c1c29259397c8abd68
anhhng <anhhng141@gmail.com>
2018-05-03 15:36:07 -0700

    update test

--- services/core/DNP3Agent/tests/test_a.py
+++ services/core/DNP3Agent/tests/test_a.py
@@ -1,58 +1,87 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python sw=4 ts=4 sts=4 et:
+#
+# Copyright 2018, eightminuteenergy / Kisensum.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Neither eightminuteenergy nor Kisensum, nor any of their
+# employees, nor any jurisdiction or organization that has cooperated in the
+# development of these materials, makes any warranty, express or
+# implied, or assumes any legal liability or responsibility for the accuracy,
+# completeness, or usefulness or any information, apparatus, product,
+# software, or process disclosed, or represents that its use would not infringe
+# privately owned rights. Reference herein to any specific commercial product,
+# process, or service by trade name, trademark, manufacturer, or otherwise
+# does not necessarily constitute or imply its endorsement, recommendation, or
+# favoring by eightminuteenergy or Kisensum.
+# }}}
+
 import gevent
 import pytest
+import json
+import os
 import time
 
-from volttrontesting.utils.platformwrapper import start_wrapper_platform, PlatformWrapper
+from pydnp3 import asiodnp3, asiopal, opendnp3, openpal
 
+from volttron.platform import get_services_core
+from services.core.DNP3Agent.tests.mesa_master import MesaMasterApplication
+from services.core.DNP3Agent.base_dnp3_agent import PointDefinitions
+from services.core.DNP3Agent.tests.MesaTestAgent.testagent.agent import MesaTestAgent
 
-@pytest.fixture(scope="module")
-def setup_instances():
+FILTERS = opendnp3.levels.NORMAL | opendnp3.levels.ALL_COMMS
+HOST = "127.0.0.1"
+LOCAL = "0.0.0.0"
+PORT = 20000
 
-    inst1 = PlatformWrapper()
-    inst2 = PlatformWrapper()
+MESA_AGENT_ID = 'mesaagent'
 
-    start_wrapper_platform(inst1)
-    start_wrapper_platform(inst2)
+POINT_DEFINITIONS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'opendnp3_data.config'))
+FUNCTION_DEFINITIONS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'mesa_functions.yaml'))
 
-    yield inst1, inst2
+pdefs = PointDefinitions(POINT_DEFINITIONS_PATH)
 
-    inst1.shutdown_platform()
-    inst2.shutdown_platform()
+input_group_map = {
+    1: "Binary",
+    2: "Binary",
+    30: "Analog",
+    31: "Analog",
+    32: "Analog",
+    33: "Analog",
+    34: "Analog"
+}
 
+MESA_AGENT_CONFIG = {
+    "point_definitions_path": POINT_DEFINITIONS_PATH,
+    "function_definitions_path": FUNCTION_DEFINITIONS_PATH,
+    "point_topic": "mesa/point",
+    "outstation_config": {
+        "database_sizes": 100,
+        "log_levels": "NORMAL | ALL_APP_COMMS"
+    },
+    "local_ip": "0.0.0.0",
+    "port": 20000
+}
 
-@pytest.mark.wrapper
-def test_can_install_listener(volttron_instance):
-    clear_messages()
-    vi = volttron_instance
-    assert vi is not None
-    assert vi.is_running()
+web_address = ""
 
-    auuid = vi.install_agent(agent_dir='/Users/anhnguyen/repos/kisensum-volttron/volttron/services/core/DNP3Agent/tests/ControlAgent',
-                             start=False)
-    assert auuid is not None
-    started = vi.start_agent(auuid)
-    print('STARTED: ', started)
-    listening = vi.build_agent()
-    listening.vip.pubsub.subscribe(peer='pubsub',
-                                   prefix='heartbeat/controlagent',
-                                   callback=onmessage)
-    # sleep for 10 seconds and at least one heartbeat should have been
-    # published
-    # because it's set to 5 seconds.
-    time_start = time.time()
-
-    print('Awaiting heartbeat response.')
-    while not messages_contains_prefix(
-            'heartbeat/controlagent') and time.time() < time_start + 10:
-        gevent.sleep(0.2)
-
-    assert messages_contains_prefix('heartbeat/controlagent')
-
-    stopped = vi.stop_agent(auuid)
-    print('STOPPED: ', stopped)
-    removed = vi.remove_agent(auuid)
-    print('REMOVED: ', removed)
-
+MESA_TEST_AGENT_CONFIG = {
+    "mesaagent_id": "mesaagent_test",
+    "point_topic": "mesa/point",
+    "function_topic": "mesa/function",
+    "outstation_status_topic": "mesa/outstation_status"
+}
 
 messages = {}
 
@@ -66,26 +95,149 @@ def clear_messages():
     messages = {}
 
 
-def messages_contains_prefix(prefix):
-    global messages
-    return any(map(lambda x: x.startswith(prefix), messages.keys()))
+@pytest.fixture(scope="module")
+def agent(request, volttron_instance_module_web):
+    """Build the test agent for rpc call."""
+
+    test_agent = volttron_instance_module_web.build_agent()
+
+    print('Installing DNP3Agent')
+    agent_id = volttron_instance_module_web.install_agent(agent_dir=get_services_core("DNP3Agent"),
+                                                          config_file=MESA_AGENT_CONFIG,
+                                                          vip_identity=MESA_AGENT_ID,
+                                                          start=True)
+
+    global web_address
+    web_address = volttron_instance_module_web.bind_web_address
+
+    test_agent.vip.pubsub.subscribe(peer='pubsub',
+                                    prefix='mesa/function',
+                                    callback=onmessage)
+
+    def stop():
+        """Stop test agent."""
+        volttron_instance_module_web.stop_agent(agent_id)
+        test_agent.core.stop()
+
+    gevent.sleep(3)        # wait for agents and devices to start
+
+    request.addfinalizer(stop)
+
+    return test_agent
 
 
-@pytest.mark.wrapper
-def test_can_publish(volttron_instance):
-    global messages
-    clear_messages()
-    vi = volttron_instance
-    agent = vi.build_agent()
-    #    gevent.sleep(0)
-    agent.vip.pubsub.subscribe(peer='pubsub', prefix='test/world',
-                               callback=onmessage).get(timeout=5)
+@pytest.fixture(scope="module")
+def run_master():
+    return MesaMasterApplication(local_ip=MESA_AGENT_CONFIG['local_ip'],
+                                 port=MESA_AGENT_CONFIG['port'])
 
-    agent_publisher = vi.build_agent()
-    #    gevent.sleep(0)
-    agent_publisher.vip.pubsub.publish(peer='pubsub', topic='test/world',
-                                       message='got data')
-    # sleep so that the message bus can actually do some work before we
-    # eveluate the global messages.
-    gevent.sleep(0.1)
-    assert messages['test/world']['message'] == 'got data'
+
+class TestMesaAgent:
+    """Regression tests for the Mesa Agent."""
+
+    @staticmethod
+    def get_point(agent, point_name):
+        """Ask DNP3Agent for a point value for a DNP3 resource."""
+        return agent.vip.rpc.call(MESA_AGENT_ID, 'get_point', point_name).get(timeout=10)
+
+    @staticmethod
+    def get_point_by_index(agent, group, index):
+        """Ask DNP3Agent for a point value for a DNP3 resource."""
+        return agent.vip.rpc.call(MESA_AGENT_ID, 'get_point_by_index', group, index).get(timeout=10)
+
+    @staticmethod
+    def set_point(agent, point_name, value):
+        """Use DNP3Agent to set a point value for a DNP3 resource."""
+        return agent.vip.rpc.call(MESA_AGENT_ID, 'set_point', point_name, value).get(timeout=10)
+
+    @staticmethod
+    def send_points(master, send_json_path):
+        """Master loads points from json and send them to mesa agent."""
+        try:
+            master.send_function_test(func_test_path=send_json_path)
+        except Exception as e:
+            print("exception {}".format(e))
+
+    @staticmethod
+    def get_value_from_master(master, point_name):
+        """Get value of the point from master after being set by test agent."""
+        try:
+            pdef = pdefs.point_named(point_name)
+            group = input_group_map[pdef.group]
+            index = pdef.index
+            return master.soe_handler.result[group][index]
+        except KeyError:
+            return None
+
+    def run_test(self, master, agent, json_file, support_point='', fail_state=False):
+        """Test get points to confirm if points is set correctly by master."""
+
+        if support_point:
+            if fail_state:
+                self.set_point(agent, support_point, False)
+            else:
+                self.set_point(agent, support_point, True)
+            time.sleep(1)
+
+        send_json_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'sample_json', json_file))
+        self.send_points(master, send_json_path)
+
+        send_json = json.load(open(send_json_path))
+
+        for point_name in send_json.keys():
+            if point_name not in ["name", "function_id", "function_name", "CurveStart-X"]:
+
+                pdef = pdefs.point_named(point_name)
+
+                if pdef.type == "array":
+                    group = pdef.group
+                    index = pdef.index
+
+                    for d in send_json[point_name]:
+                        for point in pdef.array_points:
+                            get_point = self.get_point_by_index(agent, group, index)
+                            if fail_state:
+                                assert get_point == None
+                            else:
+                                assert get_point == d[point["name"]]
+                            index += 1
+                else:
+                    get_point = self.get_point(agent, point_name)
+                    if fail_state:
+                        assert get_point == None
+                    else:
+                        assert get_point == send_json[point_name]
+
+    def test_fail_charge_discharge(self, run_master, agent):
+        """Do not set the support point to True"""
+        self.run_test(run_master, agent, 'charge_discharge.json', "Supports Charge/Discharge Mode", True)
+
+    def test_charge_discharge(self, run_master, agent):
+        """Test function charge_discharge_mode."""
+
+        self.run_test(run_master, agent, 'charge_discharge.json', "Supports Charge/Discharge Mode")
+
+        self.set_point(agent, "DCHD.WinTms (in)", 45)
+        assert self.get_value_from_master(run_master, "DCHD.WinTms (in)") == 45
+
+        assert messages['mesa/function']['message']['points'] == {
+            "DCHD.WinTms (out)": 10,
+            "DCHD.RmpTms (out)": 12,
+            "DCHD.RevtTms (out)": 13,
+            "DCHD.WTgt (out)": 14,
+            "DCHD.RmpUpRte (out)": 15,
+            "DCHD.RmpDnRte (out)": 16,
+            "DCHD.ChaRmpUpRte (out)": 17,
+            "DCHD.ChaRmpDnRte (out)": 18,
+            "DCHD.ModPrty (out)": 19,
+            "DCHD.VArAct (out)": 20,
+            "DCHD.ModEna": True
+        }
+
+    def xxx_test_curve(self, run_master, agent):
+        """Test function curve_function."""
+        self.run_test(run_master, agent, 'curve.json')
+
+    # def test_inverter(self, run_master, agent):
+    #     """Test inverter function"""
+    #     self.run_test(run_master, agent, 'inverter.json')
